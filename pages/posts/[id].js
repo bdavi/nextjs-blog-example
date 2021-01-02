f0e67c6456362df4926072edcbf770724e380550
Brian Davies <brian@briandavies.me>
2021-01-01 19:03:32 -0700

    Typo

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -30,7 +30,7 @@ export default function Post({ postData }) {
       </Head>
 
       <article>
-        <h1 clasName={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
 
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
